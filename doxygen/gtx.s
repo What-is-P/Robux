9805792e7d9cb729a6d1eafbd6c2671daf852530
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-02-16 21:30:21 -0300

    Update gtx.s

--- doxygen/gtx.s
+++ doxygen/gtx.s
@@ -1,1 +1,16 @@
- 
+local <gtx>
+#include <RBX_SERVER.2016>
+#include <RBX_CLIENT.latest-sapling-rc.0.1mmd.DirectX>
+
+if s =< DirectX (mmd, gtx);
+
+return O, BAAL.
+
+create="1, 0m"
+
+Tex="0"
+
+Rest="1"
+
+
+
